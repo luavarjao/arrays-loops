feb33d1624eb92087fd49fd504d322d9ca2f7094
Luanna <lua.varjao@gmail.com>
2023-07-24 18:14:27 -0300

    Resolução Ex 03

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,14 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = ["A", "a", "B", "C", "a", "b"];
+
+let quantidade = 0;
+
+for (let vogal of letras){
+    if (vogal === "e" || vogal === "E"){
+        quantidade ++;
+    }
+}
+if (quantidade>0){
+    console.log(`Foram encontradas ${quantidade} letras "E" ou "e".`);
+} else {
+    console.log("Nenhuma letra E ou e foi encontrada.");
+}
