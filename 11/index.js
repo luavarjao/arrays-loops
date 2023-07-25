b8f0fab7de069b6895fceda7f410b7b69736d222
Luanna <lua.varjao@gmail.com>
2023-07-25 17:44:13 -0300

    Resolução Ex 11

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,11 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 10, 17, 26, 34, 118, 245];
 
+const numeros =[];
+
+for (let i of original){
+    if (i >= 10 && i <= 20 || i >= 100){
+        numeros.push(i);
+    }
+}
+
+console.log(numeros);
