3e2eeed84ebb119b11695b46811340c18e489b19
Luanna <lua.varjao@gmail.com>
2023-07-25 08:02:10 -0300

    Resolução Ex 03

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,16 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [10, 22, 14, 87, 12, 284];
 
+let x = 0;
+
+let encontrado = false;
+
+for (let i of numeros){
+    if (i === 10){
+console.log(`O número 10 está no índice ${x}`);
+encontrado = true;
+    }
+    x++;
+}
+if (!encontrado){
+    console.log(-1);
+}
