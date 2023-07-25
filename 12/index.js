1dd9a707004cba2b3feebf85112d5a4d26b3e3af
Luanna <lua.varjao@gmail.com>
2023-07-25 17:50:43 -0300

    Resolução Ex 12

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,13 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+for (let i of filaDeDentro){
+    if (filaDeDentro.length < 5){
+        filaDeDentro.push(filaDeFora[0]);
+        filaDeFora.shift();
+    }
+}
+
+console.log(`Na fila de dentro se encontram ${filaDeDentro.length} pessoas: ${filaDeDentro}`);
+console.log(`Na fila de fora se encontram ${filaDeFora.length} pessoas: ${filaDeFora} `)
+
