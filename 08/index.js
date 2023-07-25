70eec19ce59b6a10a96fcc86e66f8d1239283958
Luanna <lua.varjao@gmail.com>
2023-07-25 17:27:59 -0300

    Resolução Ex 08

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,13 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+const numeros = [3, 24, 111, 8, 11, 7, 32];
+
+let x = 0;
+
+let maior = 0;
+
+for (let i of numeros){
+    if (i > maior){
+        maior = i;
+    }
+}
+
+console.log(maior);
