f5d4f7a89c1fc2bba75b098d1273377b6f535be4
Luanna <lua.varjao@gmail.com>
2023-07-25 16:44:29 -0300

    Resolução Ex 05

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,14 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1, 4, 12, 21, 13, 88, 112];
+
+let pares = [];
+let impares =[];
+
+for (let i of original){
+    if(i % 2 === 0){
+        pares.push(i);
+    } 
+}
+console.log(pares);
+
+const p  = original.filter (number => number %2 === 0);
+console.log(p);
