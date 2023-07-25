af81c822c2c2fec2ede99578339fdac057cf7265
Luanna <lua.varjao@gmail.com>
2023-07-25 17:23:38 -0300

    Resolução Ex 07

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,12 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+
+let nomesComA =[];
+
+for (let i of nomes){
+    if (i[0] === "A" || i[0] === "a"){
+        nomesComA.push(i);
+    }
+}
+
+console.log(nomesComA);
+
