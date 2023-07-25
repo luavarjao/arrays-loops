df90dd89d3d061649ecfe7e3fb140c0f501a0156
Luanna <lua.varjao@gmail.com>
2023-07-25 17:31:58 -0300

    Resolução Ex 09

--- 09/index.js
+++ 09/index.js
@@ -1,2 +1,7 @@
-
 const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
+
+filaDePedidos.push("pedido 15");
+console.log(filaDePedidos);
+
+filaDePedidos.shift();
+console.log(filaDePedidos);
