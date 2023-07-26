3266993fcc84af9c31e7d5aa5952d945aaacd8b2
Luanna <lua.varjao@gmail.com>
2023-07-26 20:07:10 -0300

    Resolução Ex 14

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,14 @@
-const arrayA = [5, 32, 3, 44, 1];
+const arrayA = [5, 33, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
+
+let x = 0;
+
+for (let i of arrayA){
+    if(i < arrayB[x]){
+        console.log(i);
+    } else {
+        console.log(arrayB[x]);
+    }
+    x++;
+}
 
